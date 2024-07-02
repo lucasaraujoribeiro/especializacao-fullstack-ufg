c9784fec3965215aa969dacae7bf50347100d0bd
Lucas Araujo <lucasdearaujor@gmail.com>
2024-07-02 10:25:48 -0300

    atividade pratica 02

--- fundamentos-backend/atividade-pratica-01-calculadora-servlet/src/main/java/br/inf/ufg/backend/CalculadoraServlet.java
+++ fundamentos-backend/atividade-pratica-01-calculadora-servlet/src/main/java/br/inf/ufg/backend/CalculadoraServlet.java
@@ -1,11 +1,11 @@
 package br.inf.ufg.backend;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class CalculadoraServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
