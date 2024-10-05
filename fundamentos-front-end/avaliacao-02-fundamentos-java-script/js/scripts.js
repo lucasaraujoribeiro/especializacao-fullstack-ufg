d20fca62d70cbf63b5088830a50c65300dad41ba
Lucas Araujo <lucasdearaujor@gmail.com>
2024-10-05 14:59:07 -0300

    Avaliação 02 - Fundamentos de Front End

--- fundamentos-front-end/avaliacao-02-fundamentos-java-script/js/scripts.js
+++ fundamentos-front-end/avaliacao-02-fundamentos-java-script/js/scripts.js
@@ -31,6 +31,24 @@ function contarAteN() {
     res.innerHTML += `<div class="contagem"> <span>Contagem de 1 até ${n} é ${contagem} <span></div>`
 }
 
+function calcularIdade() {
+    const anoNascimento = Number(window.prompt('Informe o ano de nascimento: '));
+    const anoAtual = new Date().getFullYear();
+
+    let msgResultado = '';
+    if (!anoNascimento || anoNascimento <= 0) {
+        msgResultado = `Ano de nascimento invalido ou não informado!`
+    } else if (anoNascimento <= anoAtual) {
+       msgResultado = `<p>Quem nasceu em ${anoNascimento} irá completar ${anoAtual - anoNascimento}  anos em ${anoAtual} </p>`;
+    } else {
+        msgResultado = `O ano de nascimento informado: ${anoNascimento}, é maior que o ano atual: ${anoAtual}`;
+    }
+
+    let res = document.querySelector('section#resultadoMediaAluno')
+    res.innerHTML = '<h3>Resultados<h3>';
+    res.innerHTML += msgResultado;
+}
+
 function montarHtmlTabelaResultados(resultados) {
     let html = '<h3>Resultado</h3>';
     html += '<table class="tbResultados">';
