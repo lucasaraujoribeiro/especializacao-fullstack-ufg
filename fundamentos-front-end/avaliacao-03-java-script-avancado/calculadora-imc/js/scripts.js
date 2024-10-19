49ac6b5f1eb683fd8c69a6c1dba659b4507e34e0
Lucas Araujo <lucasdearaujor@gmail.com>
2024-10-19 17:14:20 -0300

    Avaliacao 3 - Fundamentos programacao frontend - JavaScript Avancado

---
+++ fundamentos-front-end/avaliacao-03-java-script-avancado/calculadora-imc/js/scripts.js
@@ -0,0 +1,132 @@
+const btnVerResultado = document.querySelector("#btnCalcularImc");
+btnVerResultado.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcularImc()
+});
+
+const btnLimpar = document.querySelector('#btnLimpar');
+btnLimpar.addEventListener('click', (e) => {
+    e.preventDefault();
+    limparForm();
+});
+
+const elMsgAlert = document.querySelector("#alertmsg");
+
+function calcularImc() {
+    limparMsg();
+
+    let altura = document.querySelector("#altura").value;
+    let peso = document.querySelector("#peso").value;
+
+    if (isEmpty(altura)) {
+        exibirMensagem(`A altura deve ser informada!`, 'danger')
+        return;
+    }
+
+    altura = altura.replace('.','').replace(',','.');
+    if (!isNumber(altura)) {
+        exibirMensagem(`A altura é inválida! ${altura}`, 'danger')
+        return;
+    }
+
+    if (isEmpty(peso)) {
+        exibirMensagem(`A peso deve ser informado!`, 'danger')
+        return;
+    }
+
+    peso = peso.replace('.','').replace(',','.');
+    if (!isNumber(peso)) {
+        exibirMensagem(`A peso é inválido! ${peso}`, 'danger')
+        return;
+    }
+
+    altura = Number(altura);
+    peso = Number(peso);
+
+    altura = altura * altura;
+    const resultado = peso / altura;
+
+    let situacao = '';
+    let grauObesidade = '';
+    if (resultado < 16) {
+        situacao = 'magreza grave';
+    } else if (resultado < 17) {
+        situacao = 'magreza moderada';
+    } else if (resultado < 18.6) {
+        situacao = 'magreza leve';
+    } else if (resultado < 25) {
+        situacao = 'peso ideal';
+    } else if (resultado < 30) {
+        situacao = 'sobrepeso';
+    } else if (resultado < 35) {
+        situacao = 'Obesidade grau I';
+        grauObesidade = 1;
+    } else if (resultado < 40) {
+        situacao = 'Obesidade grau II ou severa';
+        grauObesidade = 2;
+    } else {
+        situacao = 'Obesidade grau III ou mórbida';
+        grauObesidade = 3;
+    }
+
+    document.querySelector('#valor-imc'). value = resultado.toFixed(2);
+    document.querySelector('#situacao-imc'). value = situacao;
+    document.querySelector('#grau-obesidade-imc'). value = grauObesidade;
+
+    exibirMensagem('Calculo realizado com sucesso!', 'success');
+}
+
+function limparForm() {
+    document.querySelector('#altura').value = '';
+    document.querySelector('#peso').value = '';
+    document.querySelector('#valor-imc'). value = '';
+    document.querySelector('#situacao-imc'). value = '';
+    document.querySelector('#grau-obesidade-imc'). value = '';
+}
+
+function limparMsg() {
+    elMsgAlert.style.display = 'none';
+    elMsgAlert.textContent = '';
+    for (let n of elMsgAlert.childNodes) {
+        console.log(`Node msg: ${n.textContent}`);
+        n.remove();
+    }
+}
+
+function exibirMensagem(msg, tipo) {
+    let tipoMsgDescricao = '';
+
+    switch (tipo) {
+        case 'sucess':
+            tipoMsgDescricao = 'Sucesso!';
+            break;
+        case 'danger':
+            tipoMsgDescricao = 'Erro!';
+            break;
+        case 'warning':
+            tipoMsgDescricao = 'Aviso!';
+            break;
+        case 'info':
+            tipoMsgDescricao = 'Informação';
+            break;
+    }
+
+    const strong = document.createElement("strong");
+    strong.appendChild(document.createTextNode(tipoMsgDescricao));
+
+    elMsgAlert.style.display = '';
+    elMsgAlert.setAttribute('class', `alert alert-${tipo}`);
+    elMsgAlert.appendChild(strong);
+    elMsgAlert.appendChild(document.createTextNode(` ${msg}`));
+
+    setTimeout(limparMsg, 2000);
+}
+
+function isEmpty(value) {
+    return value === undefined || value === null || value === '';
+}
+
+function isNumber(value) {
+    const regex = /^-?\d+(\.\d+)?$/;
+    return regex.test(value);
+}
---
+++ fundamentos-front-end/avaliacao-03-java-script-avancado/calculadora/js/scripts.js
@@ -0,0 +1,114 @@
+const btnVerResultado = document.querySelector("#btn-resultado");
+btnVerResultado.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcular()
+});
+
+const elMsgAlert = document.querySelector("#alertmsg");
+
+function calcular() {
+    const iptResultado = document.querySelector("#resultado");
+    iptResultado.value = 0;
+
+    limparMsg();
+
+    let n1 = document.querySelector("#n1").value;
+    let n2 = document.querySelector("#n2").value;
+    const operacao = document.querySelector("#op").value;
+
+    if (isEmpty(n1)) {
+        exibirMensagem(`O número 1 deve ser informado!`, 'danger')
+        return;
+    }
+
+    if (!isNumber(n1)) {
+        exibirMensagem(`O Numero 1 inválido! ${n1}`, 'danger')
+        return;
+    }
+
+    if (isEmpty(n2)) {
+        exibirMensagem(`O número 2 deve ser informado!`, 'danger')
+        return;
+    }
+    btnVerResultado.in
+
+    if (!isNumber(n2)) {
+        exibirMensagem(`O Numero 2 inválido! ${n2}`, 'danger')
+        return;
+    }
+
+    n1 = Number(n1);
+    n2 = Number(n2);
+
+    let resultado;
+    switch (operacao) {
+        case '+':
+            resultado = n1 + n2;
+            break;
+        case '-':
+            resultado = n1 - n2;
+            break;
+        case '*':
+            resultado = n1 * n2;
+            break;
+        case '/':
+            if (n2 === 0) {
+                exibirMensagem(`Não é possivel fazer divisão por zero!`, 'danger');
+                return;
+            }
+            resultado = n1 / n2;
+            break;
+        default:
+            break;
+    }
+
+    iptResultado.value = resultado;
+    exibirMensagem(`Calculo realizado com sucesso!`, 'success')
+}
+
+function limparMsg() {
+    elMsgAlert.style.display = 'none';
+    elMsgAlert.textContent = '';
+    for (let n of elMsgAlert.childNodes) {
+        console.log(`Node msg: ${n.textContent}`);
+        n.remove();
+    }
+}
+
+function exibirMensagem(msg, tipo) {
+    let tipoMsgDescricao = '';
+
+    switch (tipo) {
+        case 'sucess':
+            tipoMsgDescricao = 'Sucesso!';
+            break;
+        case 'danger':
+            tipoMsgDescricao = 'Erro!';
+            break;
+        case 'warning':
+            tipoMsgDescricao = 'Aviso!';
+            break;
+        case 'info':
+            tipoMsgDescricao = 'Informação';
+            break;
+    }
+
+    const strong = document.createElement("strong");
+    strong.appendChild(document.createTextNode(tipoMsgDescricao));
+
+    elMsgAlert.style.display = '';
+    elMsgAlert.setAttribute('class', `alert alert-${tipo}`);
+    elMsgAlert.appendChild(strong);
+    elMsgAlert.appendChild(document.createTextNode(` ${msg}`));
+
+    setTimeout(limparMsg, 2000);
+}
+
+function isEmpty(value) {
+    return value === undefined || value === null || value === '';
+}
+
+function isNumber(value) {
+    const regex = /^-?\d+(\.\d+)?$/;
+    return regex.test(value);
+}
